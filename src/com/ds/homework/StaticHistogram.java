a6e98961e4cb3214e68b9de6b462407bfd852dbe
M-S-A <mohamdshafie@gmail.com>
2019-10-04 01:54:26 -0500

    Homework

---
+++ src/com/ds/homework/StaticHistogram.java
@@ -0,0 +1,38 @@
+package com.ds.homework;
+
+public class StaticHistogram {
+
+
+
+    public static void main(String[] args) {
+
+        // Given the definition of histogram, simply a bar diagram.
+        // For details see link
+        // https://en.wikipedia.org/wiki/Histogram
+        // We want to show a static Histogram with println("*") method that
+        // represent 5 bars histogram values (13, 9, 2, 10, 5)
+        //
+        // The Illustration below is a representation for 5-bars histogram of values (4, 1, 1, 1, 2)
+        //
+        // *
+        // *
+        // *   *
+        // *****
+
+        //  PUT YOUR CODE HERE
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
