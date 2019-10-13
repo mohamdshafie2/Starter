f8702b06dbd7174030e05d8ea7ea5bffd7c71c5e
M-S-A <mohamdshafie@gmail.com>
2019-10-13 18:17:39 -0500

    changes of session#3 String handling , extra

--- src/com/ds/homework/Worms.java
+++ src/com/ds/homework/Worms.java
@@ -16,21 +16,50 @@ public class Worms {
         // 2- use string split.
         // 3- use int to String (notTaco)
 
-        String[] parts = "taco - bell".split("-") ;
-        for (int i=0 ; i<parts.length ; i++ ){
-            System.out.println(parts[i]);
-        }
+//        String[] parts = "taco - bell".split("-") ;
+//        for (int i=0 ; i<parts.length ; i++ ){
+//            System.out.println(parts[i]);
+//        }
 
-        String notTaco = 200+"";
-        System.out.println(notTaco);
+
+
+        String strToSplit = "we have a string here 0 split it by something";
+
+        String a= "abc";
+        String b = "def";
+
+        String[] _2_parts = strToSplit.split(" 0 ") ;
+        for (int i=0 ; i<_2_parts.length ; i++ ){
+            System.out.println( _2_parts[i]   );
+        }
+//        String notTaco = 200+"";
+//        System.out.println(notTaco);
 
 
 //        for(int i=1;  i <= 1000000 ; i ++){
 //
 //        }
 
+        System.out.println(a+b);
+        System.out.println(a.concat(b));
 
-// for demonstration purposes only .... you should see this when you pull from git 
+        if(a.compareTo(b) == 0 ){
+           System.out.println("strings are equal");
+
+        }else {
+           System.out.println("strings are not equal ");
+        }
+
+
+
+        // convert string to int
+        String aaa = "90";
+        int val = Integer.parseInt(aaa);
+
+
+
+
+
 
 
     }
